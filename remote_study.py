dd5e4a9250897bffa965c3929f92145dde641d40
JWellman4 <52516150+JWellman4@users.noreply.github.com>
2024-03-25 18:46:57 -0400

    Update remote_study.py

--- remote_study.py
+++ remote_study.py
@@ -13,7 +13,7 @@ def load_data(file_path):
     return pd.read_csv(file_path)
 
 # Load the data
-df = load_data('studysession/Correlation.csv')
+df = load_data('Correlation.csv')
 
 # Transforming Data
 ## create a dictionary of replacements
